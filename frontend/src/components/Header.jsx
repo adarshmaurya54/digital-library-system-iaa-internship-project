b8fb7b3b58e8632accbbff080c834267362a1b3f
Adarsh Maurya <adarshmaurya0022@gmail.com>
2025-07-11 15:38:56 +0530

    hamburger design

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,37 +1,45 @@
-import React from "react";
 import { MdOutlineLocalLibrary } from "react-icons/md";
 import img_dgca from "../assets/img_dgca.png"
 import img_logo from "../assets/img_logo.png"
 import img_bcab from "../assets/img_bcab.png"
 import img_airports from "../assets/img_airports.png"
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { LiaTimesSolid } from "react-icons/lia";
+import { useState } from "react";
+
 
 
 export default function Header() {
+  const [hamb, setHamb] = useState(false)
   return (
     <header className="flex flex-col">
-      <div className="bg-gradient-to-r from-[#0257a7] to-[#9bceed] flex justify-between items-center md:px-10 px-5 py-3">
+      <div className="bg-gradient-to-r from-[#0257a7] to-[#9bceed] flex justify-between  items-center md:px-10 px-5 py-3">
 
         <div className="flex flex-col md:items-end space-y-3">
           <div className="flex items-end space-x-3">
             <img
               src={img_logo}
               alt="IAA Logo"
-              className="h-11"
+              className="md:h-11 h-9"
             />
             <div className="text-white">
               <h1 className="md:text-lg text-sm leading-4 font-semibold">Indian Aviation Academy</h1>
               <p className="md:text-sm text-xs leading-4">Nurturing Aviation for the Future</p>
             </div>
           </div>
-          <div class="flex items-center gap-3 ms-[-2px]">
+          <div className="md:hidden flex items-center gap-3 ms-[-2px]">
             <MdOutlineLocalLibrary className="inline text-3xl text-white" />
             <p className="capitalize font-semibold text-white">digital library system</p>
           </div>
         </div>
 
+        <div  onClick={() => setHamb(true)}  className="md:hidden flex cursor-pointer">
+          <GiHamburgerMenu className="text-white text-2xl"/>
+        </div>
+
         <div className="text-white font-semibold text-lg md:flex flex-col items-center hidden">
-          <MdOutlineLocalLibrary className="inline text-3xl text-yellow-300" /> Digital Library System
+          <MdOutlineLocalLibrary className="inline text-3xl text-yellow-300" />Digital Library System
         </div>
 
         {/* Right: Government, DGCA, AAI logos */}
@@ -53,41 +61,49 @@ export default function Header() {
           />
         </div>
       </div>
-      <div className="bg-white shadow-sm px-6">
-        <nav className="flex justify-between items-center max-w-7xl mx-auto">
+      <div className={`bg-white/90 backdrop-blur-md border-l border-gray-200 py-8 z-50 md:z-[unset] fixed md:relative top-0 right-0 md:h-auto h-full w-[80%] max-w-xs ${!hamb ? 'translate-x-full' : 'translate-x-0'} transition-transform duration-300 ease-in-out md:w-full shadow-2xl px-6 overflow-y-auto`}>
+        <div className="text-end">
+          <LiaTimesSolid onClick={() => setHamb(false)} className="inline cursor-pointer text-2xl"/>
+        </div>
+        <nav className="flex md:flex-row flex-col justify-between md:items-center max-w-7xl mx-auto">
           {/* Left links */}
-          <div className="flex gap-6">
+          <div className="flex md:flex-row flex-col md:gap-6">
             <Link
+              onClick={() => setHamb(false)}
               to="/"
-              className="relative py-2 border-white border-b-[3px] hover:border-yellow-400 transition"
+              className="relative py-2 w-fit border-transparent border-b-[3px] hover:border-yellow-400 transition"
             >
               Home
             </Link>
             <Link
+              onClick={() => setHamb(false)}
               to="/about"
-              className="relative py-2 border-white border-b-[3px] hover:border-yellow-400 transition"
+              className="relative py-2 w-fit border-transparent border-b-[3px] hover:border-yellow-400 transition"
             >
               About
             </Link>
             <Link
+              onClick={() => setHamb(false)}
               to="/contact"
-              className="relative py-2 border-white border-b-[3px] hover:border-yellow-400 transition"
+              className="relative py-2 w-fit border-transparent border-b-[3px] hover:border-yellow-400 transition"
             >
               Contact Us
             </Link>
           </div>
 
           {/* Right links */}
-          <div className="flex gap-6">
+          <div className="flex md:flex-row flex-col md:gap-6">
             <Link
+              onClick={() => setHamb(false)}
               to="/login"
-              className="relative py-2 border-white border-b-[3px] hover:border-yellow-400 transition"
+              className="relative py-2 w-fit border-transparent border-b-[3px] hover:border-yellow-400 transition"
             >
               Login
             </Link>
             <Link
+              onClick={() => setHamb(false)}
               to="/signup"
-              className="relative py-2 border-white border-b-[3px] hover:border-yellow-400 transition"
+              className="relative py-2 w-fit border-transparent border-b-[3px] hover:border-yellow-400 transition"
             >
               Signup
             </Link>
